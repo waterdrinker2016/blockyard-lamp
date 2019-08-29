9800d2fc70cd7ec2b5c15028e792db85b714678e
Blockyard-Code <53713564+Blockyard-Code@users.noreply.github.com>
2019-08-29 19:50:25 -0400

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -5,10 +5,13 @@
 # find your server's ip address: ip addr show eth0 | grep inet | awk '{ print $2; }' | sed 's/\/.*$//' 
 # OR:  sudo apt install curl
 # curl http://icanhazip.com
+# To execute this script use: wget -O - https://github.com/Blockyard-Code/blockyard-lamp.git | bash
 
 if [ "`lsb_release -is`" == "Ubuntu" ] || [ "`lsb_release -is`" == "Debian" ]
 then
     sudo apt update && sudo apt upgrade
+#install git
+    sudo apt install git
 #webserver
     sudo apt install apache2
 #database
