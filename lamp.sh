40b250b8111ccc605a8380d86d629411924b1716
Blockyard-Code <53713564+Blockyard-Code@users.noreply.github.com>
2019-08-29 19:04:59 -0400

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 # by Brett Garman
+# Intended use: Ubuntu Server 18
+#
+# find your server's ip address: ip addr show eth0 | grep inet | awk '{ print $2; }' | sed 's/\/.*$//' 
+# OR:  sudo apt install curl
+# curl http://icanhazip.com
 
 if [ "`lsb_release -is`" == "Ubuntu" ] || [ "`lsb_release -is`" == "Debian" ]
 then
-    sudo apt-get -y install mysql-server mysql-client mysql-workbench libmysqld-dev;
-    sudo apt-get -y install apache2 php7 libapache2-mod-php5 php5-mcrypt phpmyadmin;
-    sudo chmod 755 -R /var/www/;
-    sudo printf "<?php\nphpinfo();\n?>" > /var/www/html/info.php;
+    sudo apt update && sudo apt upgrade
+#webserver
+    sudo apt install apache2
+#database
+    sudo apt install mysql-server mysql-client mysql-workbench libmysqld-dev;
+#remove dangerous security issues and lock down system
+    sudo mysql_secure_installation
+#coding language for wordpress and others
+    sudo apt install php libapache2-mod-php php-mysql 
+        #(another possible option)sudo apt install php7.2-fpm php7.2-common php7.2-mysql php7.2-gmp php7.2-curl php7.2-intl php7.2-mbstring php7.2-xmlrpc php7.2-gd php7.2-bcmath php7.2-xml php7.2-cli php7.2-zip
+#restart the web server
     sudo service apache2 restart;
 
 else
